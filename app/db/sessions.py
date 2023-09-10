4fac9515b403e498acdc8cd478d99e2ca5124a81
André Freitas <freitasandre38@gmail.com>
2023-09-10 10:12:01 -0300

    feat: create basic crud folder

--- app/db/sessions.py
+++ app/db/sessions.py
@@ -6,7 +6,6 @@ SQLALCHEMY_DATABASE_URI = "sqlite:///example.db"  # 1
 
 engine = create_engine(  # 2
     SQLALCHEMY_DATABASE_URI,
-    # required for sqlite
     connect_args={"check_same_thread": False},  # 3
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) 
