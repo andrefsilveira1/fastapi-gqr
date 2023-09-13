5f9a0ec66b0ac887f9a9ee8246328900c023c3eb
André Freitas <freitasandre38@gmail.com>
2023-09-13 13:52:48 -0300

    feat: create find_best_gqr function

---
+++ app/utils/find_best_gqr.py
@@ -0,0 +1,16 @@
+def find_best_gqr(arr, limit=None):
+    result = []
+    
+    for data in arr:
+        depth = float(data['depth'])
+        gqr = calculate_gqr(data)
+        result.append({'depth': depth, 'gqr': gqr})
+    
+    result.sort(key=lambda x: x['gqr'])
+    
+    if limit:
+        greatest = result[-10:]
+    else:
+        greatest = result
+
+    return {'greatest': greatest}
